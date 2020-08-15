df641e28cb2cde7e92fe576f9fe92a2875a5ef59
pavel <geraci98.98@gmail.com>
2020-08-15 19:54:39 +0300

    add file-loader,  works with img

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import Post from "./Post";
 import "./style.css";
 import json from "./assets/json.json";
-const post = new Post("Webpac Post Title");
+import WebpackLogo from "./assets/webpack-logo.png";
+const post = new Post("Webpac Post Title", WebpackLogo);
 
 console.log("Post to String", post.toString());
+
+console.log("JSON", json);
