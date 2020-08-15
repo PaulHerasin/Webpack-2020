7641c430c72e894aa042e8aaf6a2aa306978ceb0
pavel <geraci98.98@gmail.com>
2020-08-15 19:39:31 +0300

    import JSON

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Post from "./Post";
 import "./style.css";
-
+import json from "./assets/json.json";
 const post = new Post("Webpac Post Title");
 
 console.log("Post to String", post.toString());
