61ca6d54900d37bffb24539b3af8308f652aa627
pavel <geraci98.98@gmail.com>
2020-08-15 20:40:14 +0300

    add xml-loader, xml file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Post from "./Post";
 import "./styles/style.css";
 import json from "./assets/json.json";
+import xml from "./assets/data.xml";
 import WebpackLogo from "./assets/webpack-logo.png";
 const post = new Post("Webpac Post Title", WebpackLogo);
 
 console.log("Post to String", post.toString());
 
 console.log("JSON", json);
+console.log("XML", xml);
